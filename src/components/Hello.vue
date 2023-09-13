d48a7bb072ae94dd5d718954c29297c22e1bdc94
smaru1111 <marusou20@gmail.com>
2023-09-13 21:02:05 +0900

    update いらんやつけした

---
+++ src/components/Hello.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import { ref } from "vue";
+  let count = ref(0);
+  defineProps(['title']);
+</script>
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <button @click="count++">You clicked me {{ count }} times.</button>
+  </div>
+</template>
+
+
+<style>
+</style>
