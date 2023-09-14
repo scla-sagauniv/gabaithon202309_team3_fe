99c9678dc24d35b7cb1421bd20aedbf08d6f167c
smaru1111 <marusou20@gmail.com>
2023-09-15 01:59:04 +0900

    #21 add textareaコンポーネント作成

--- src/views/Sample.vue
+++ src/views/Sample.vue
@@ -1,16 +1,21 @@
 <script setup>
 import IdeaTail from "../components/IdeaTail.vue";
 import InputForm from "../components/InputForm.vue";
+import TextAreaForm from "../components/TextAreaForm.vue";
 </script>
 
 <template>
-  <div id="app">
+  <div id="app" class="kuso">
     <h1>sample
     </h1>
     <IdeaTail/>
     <InputForm/>
+    <TextAreaForm />
   </div>
 </template>
 
 <style scoped>
+.kuso {
+  padding: 40px;
+}
 </style>
