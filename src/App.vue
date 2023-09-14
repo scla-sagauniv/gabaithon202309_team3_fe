2a9b01bd2af3cc0775ab0276b3e40ebf5e1fdd1e
kamakura0523 <suieikama0523@gmail.com>
2023-09-14 22:07:37 +0900

    #8 add ボタンコンポーネントの作成

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <script setup>
   import Hello from './components/Hello.vue';
   import BlobUpload from './components/BlobUpload.vue';
+  import Button from './components/button.vue';
 </script>
 
 <template>
-  <div id="app">
-    <Hello />
-    <BlobUpload />
+  <div class="kuro" id="app">
+      <Hello />
+      <BlobUpload />
+      <Button  class="buttonsize" button="aaa"/>
   </div>
 </template>
 
 <style scoped>
+  .kuro{
+    background-color: #444444;
+    min-height: 100vh;
+  }
+  .buttonsize{
+    text-align: center;
+    padding: 50px;
+  }
+
 </style>
