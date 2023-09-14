019e274221ea688c4bd67d79c27d902b6fae9e47
smaru1111 <marusou20@gmail.com>
2023-09-14 21:15:07 +0900

    add ルーティング機能の追加

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import router from './router'  //ルーターを router/index.js からインポート
+ 
+import './assets/main.css'  //CSS のインポート
+ 
+const app = createApp(App)  //Vue のインスタンスを生成
+ 
+app.use(router)  //Vue のインスタンスにルーターをインストールして有効化
+ 
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <div id="app">
+    <h1>home</h1>
+    <RouterLink to="/about">About</RouterLink>
+  </div>
+</template>
+
+<style scoped>
+</style>
